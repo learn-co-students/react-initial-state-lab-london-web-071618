39402c24101137ea376704318e54ba304bf264a9
Flatiron School <flatiron@Flatirons-MacBook-Air.local>
2018-09-18 14:12:35 +0100

    Jo

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,21 @@
 // your Bomb code here!
+import React, { Component } from 'react';
+
+export default class Bomb extends Component {
+    
+    constructor(props) {
+        super(props)
+        const initialCount = this.props.initialCount
+        this.state = {secondsLeft : initialCount}
+    }
+
+    render () {
+       let textToRender
+       if (this.state.secondsLeft === 0 ) {
+        textToRender = "Boom!"
+       } else {
+        textToRender = `${this.state.secondsLeft} seconds left before I go boom!`
+       }
+       return textToRender
+    }
+} 
